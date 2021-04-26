98d9605c441d684b357b46f1835b3688e7300336
trivialGenius-bard <trivialGenius.bard@gmail.com>
2021-04-26 14:32:16 +0300

    Исправление 2

--- main.rb
+++ main.rb
@@ -1,108 +1,117 @@
+# frozen_string_literal: true
+
+# Station class
 class Station
-  attr_reader :trains, 
-              :name, 
-              :fr_num, 
-              :pas_num
+  attr_reader :trains,
+              :name
+
   def initialize(name)
     @name = name
-    @trains = []                #список поездов
-    @fr_num = 0
-    @pas_num = 0
+    @trains = [] # list of trains
   end
-  #прибытие поездов
+
   def train_arrival(train)
     @trains << train
-    if train.type == "пассажирский"
-      @pas_num +=1
-    else
-      @fr_num += 1
-    end
   end
-  #отправление поезда
+
   def train_department(train)
     @trains.delete(train)
-    if train.type == "пассажирский"
-      @pas_num -= 1
-    else
-      @fr_num -= 1
-    end
+  end
+
+  def train_by(type)
+    @trains.select { |t| t.type == type }
+  end
+
+  def count_train_by(type)
+    train_by(type).length
   end
 end
+
+# Train class
 class Train
-  attr_reader :velocity, 
-              :num_of_wagons, 
-              :st_num, 
-              :type, 
-              :station,
+  attr_reader :velocity,
+              :num_of_wagons,
+              :st_num,
+              :type,
               :route,
-              :next_station,
-              :prev_station,
               :number
+
   def initialize(number, type, wagnum)
-    @number = number          #номер
+    @number = number
     @type = type
-    @num_of_wagons = wagnum     #количество вагонов
+    @num_of_wagons = wagnum
     @velocity = 0
-    @station = nil
     @route = nil
     @st_num = 0
   end
-  #ускорение
-  def accelerate(v)
-    @velocity += v
+
+  def accelerate_by(velocity)
+    @velocity += velocity
   end
-  #торможение
+
   def stop
     @velocity = 0
   end
-  #прицепление вагона
+
   def hook
-    if @velocity == 0
-      @num_of_wagons += 1
-    end
+    @num_of_wagons += 1 if @velocity.zero?
   end
-  #отцепление вагона
+
   def unhook
-    if @velocity == 0 && @num_of_wagons > 0
-      @num_of_wagons -= 1
-    end
+    @num_of_wagons -= 1 if @velocity.zero? && @num_of_wagons.positive?
   end
+
   def follow_route(route)
     @route = route
-    @station = route.stations[0]
+    @route.stations[0].train_arrival self
     @st_num = 0
   end
-  def go_to_next
-    if @st_num == @route.stations.length-1
-      #сообщение о попытке выхода за пределы маршрута
-      puts "Поезд дальше не идет, просьба выйти из вагона"   
+
+  def station
+    @route.stations[@st_num]
+  end
+
+  def move_by(num)
+    if (@st_num + num >= @route.stations.length) || (@st_num + num).negative?
+      puts 'Поезд дальше не идет, просьба выйти из вагона'
     else
-      @st_num +=1
-      @station = @route.stations[@st_num]
+      station.train_department self
+      @st_num += num
+      station.train_arrival self
     end
   end
+
+  def go_to_next
+    move_by(1)
+  end
+
   def go_to_prev
-    if @st_num == 0
-      #сообщение о попытке выхода за пределы маршрута
-      puts "Поезд дальше не идет, просьба выйти из вагона"
-    else
-      @st_num -= 1
-      @station = @route.stations[@st_num]
-    end
+    move_by(-1)
+  end
+
+  def next_station
+    @route.stations[@st_num + 1]
+  end
+
+  def prev_station
+    @route.stations[@st_num - 1]
   end
 end
+
+# Route class
 class Route
   attr_reader :stations
+
   def initialize(arr, *inter, dep)
     @arr_station = arr
     @dep_station = dep
     @stations = [arr] + inter + [dep]
   end
+
   def add_inter(station)
-    dep = @stations.pop
-    @stations<<station
-    @stations.push(dep)
+    @stations.insert(@stations.length - 1, station)
   end
+
   def remove_inter(station)
     @stations.delete(station)
   end
